7b0e3b69ed73cef5abfe467d3ad4725e358eb8af
jasarqui <jzarquilita@up.edu.ph>
2019-04-15 23:32:57 +0800

    Backtrack with Initial Board

--- nchancellors.c
+++ nchancellors.c
@@ -13,78 +13,106 @@
 // define globals
 // this is to minimize the parameters passed
 int boardsize, num_candidates, move, num_boards;
-int* nopts, *rows_with_chancellor;
+int* nopts, *rows_with_chancellor, *chancellor_position;
 int** options;
-clock_t start, end;
-double cpu_time_used;
 
 // returns the list of candidates
 void generate_candidates() {
     num_candidates = 0;
-
     int i, j;
-    for (i = boardsize; i > 0; i--) {
-        // check rook moves
+
+    // check first if row already has existing move
+    if (rows_with_chancellor[move] == 1) {
+        // check if valid
         int flag = AVAILABLE;
+
+        // check rook moves
         for (j = 1; j < move; j++) {
             // check top of stacks
-            if (i == options[j][nopts[j]]) {
+            if (chancellor_position[move] == options[j][nopts[j]]) {
                 flag = UNAVAILABLE;
                 break;
             }
         }
 
-        if (flag == UNAVAILABLE) continue;
         // check knight moves
         // check previous move
-        else if (options[move-1][nopts[move-1]] == i - 2 || options[move-1][nopts[move-1]] == i + 2) continue;
+        if (move > 1 && (options[move-1][nopts[move-1]] == chancellor_position[move] - 2 || 
+            options[move-1][nopts[move-1]] == chancellor_position[move] + 2)) flag = UNAVAILABLE;
         // check 2 moves before
-        else if (move > 2 && (options[move-2][nopts[move-2]] == i - 1 || options[move-2][nopts[move-2]] == i + 1)) continue;
-        // add as candidate
-        else {
-            options[move][num_candidates+1] = i;
+        else if (move > 2 && (options[move-2][nopts[move-2]] == chancellor_position[move] - 1 || 
+            options[move-2][nopts[move-2]] == chancellor_position[move] + 1)) flag = UNAVAILABLE;
+
+        if (flag == AVAILABLE) {
+            // printf("%d: %d\n", num_candidates+1, chancellor_position[i]);
+            options[move][num_candidates+1] = chancellor_position[move];
             num_candidates++;
         }
-    }
-}
+    } else if (rows_with_chancellor[move] == 0) {
+        for (i = boardsize; i > 0; i--) {
+            // check rook moves
+            int flag = AVAILABLE;
+            for (j = 1; j < move; j++) {
+                // check top of stacks
+                if (i == options[j][nopts[j]]) {
+                    flag = UNAVAILABLE;
+                    break;
+                }
+            }
 
-// returns all possible start
-void starting_candidates() {
-    num_candidates = 0;
-
-    int i;
-    for (i = boardsize; i > 0; i--) {
-        options[move][num_candidates+1] = i;
-        num_candidates++;
+            if (flag == UNAVAILABLE) continue;
+            // check knight moves
+            // check previous move
+            else if (move > 1 && (options[move-1][nopts[move-1]] == i - 2 || 
+                options[move-1][nopts[move-1]] == i + 2)) continue;
+            // check 2 moves before
+            else if (move > 2 && (options[move-2][nopts[move-2]] == i - 1 || 
+                options[move-2][nopts[move-2]] == i + 1)) continue;
+            // add as candidate
+            else {
+                options[move][num_candidates+1] = i;
+                num_candidates++;
+            }
+        }
     }
+    // else, there are more than one
+    // hence do nothing
 }
 
 int main() {
     // read file
     FILE* fp;
     char buffer[255];
-
     fp = fopen("board.in", "r");
+
     // get the number of boards
     fgets(buffer, 255, fp);
     num_boards = atoi(buffer);
 
-    int i, j, n;
-    // get the board
+    // declare utility variables
+    int i, j, n, invalid_board;
+    clock_t start, end;
+    double cpu_time_used;
+
+    // get the boards
     for (n = 0; n < num_boards; n++) {
         fgets(buffer, 255, fp);
         boardsize = atoi(buffer);
+        invalid_board = 0;
 
         // initialize arrays
         nopts = (int *) malloc(sizeof(int) * (boardsize + 2));
         rows_with_chancellor = (int *) malloc(sizeof(int) * (boardsize + 2));
+        chancellor_position = (int *) malloc(sizeof(int) * (boardsize + 2));
         options = (int **) malloc(sizeof(int*) * (boardsize + 2));
         for (i = 0; i < boardsize + 2; i++) {
             options[i] = (int *) malloc(sizeof(int) * (boardsize + 2));
+            // initialize to 0
             for (j = 0; j < boardsize + 2; j++) {
                 options[i][j] = 0;
             }
             nopts[i] = 0;
+            rows_with_chancellor[i] = 0;
         }
 
         // get the board initialization
@@ -95,28 +123,33 @@ int main() {
             char *token;
 
             int col = 1;
-            // splits a row/line by spaces, then assigns them to the board
+            // check for chancellors already present
             // ref: https://stackoverflow.com/questions/15961253/c-correct-usage-of-strtok-r
             for (token = strtok_r(buffer, " ", &rest); token != NULL; token = strtok_r(NULL, " ", &rest)) {
-                options[i][col] = atoi(token);
-
                 // check for edited rows
-                if (atoi(token) != 0) rows_with_chancellor[i] = TRUE;
+                if (atoi(token) != 0) {
+                    chancellor_position[i] = col;
+                    rows_with_chancellor[i]++;
+                    // check row validity
+                    if (rows_with_chancellor[i] > 1) invalid_board = TRUE;
+                }
                 col++;
             }
         }
 
         // backtrack the current board
-        printf("Boardsize: %d\n", boardsize);
-        // for (i = 1; i <= boardsize; i++) {
-        //     printf("%d ", rows_with_chancellor[i]);
-        // }
-        // printf("\n");
+        printf("Board Size: %d\n", boardsize);
 
         // backtrack utilities
         int num_of_solutions = 0;
         move = 0;
         nopts[START] = 1;
+
+        if (invalid_board == TRUE) {
+            // more than one chancellors in one row
+            printf(">> No solution found.\n");
+            continue;
+        }
 
         // backtracking
         start = clock();
@@ -130,10 +163,7 @@ int main() {
                     //     printf("(%d, %d) ", options[i][nopts[i]] - 1, i - 1);
                     // }
                     // printf("\n");
-                    num_of_solutions++;                    
-                } else if (move == 1) {
-                    starting_candidates();
-                    nopts[move] = num_candidates;
+                    num_of_solutions++;         
                 } else { // push
                     generate_candidates();
                     nopts[move] = num_candidates;    
@@ -147,7 +177,13 @@ int main() {
         end = clock();
         cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-        printf(">> %d solutions found in %f seconds.\n", num_of_solutions, cpu_time_used);
+        if (num_of_solutions == 1) {
+            printf(">> %d solution found in %f seconds.\n", num_of_solutions, cpu_time_used);
+        } else if (num_of_solutions == 0) {
+            printf(">> No solution found in %f seconds.\n", cpu_time_used);
+        } else {
+            printf(">> %d solutions found in %f seconds.\n", num_of_solutions, cpu_time_used);
+        }
         // deallocate arrays
         for (i = 0; i < boardsize + 2; i++) {
             free(options[i]);
